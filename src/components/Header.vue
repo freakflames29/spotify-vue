7622fbf8ad12289d542ff6c61a6c826eb8ce547a
Sourav Das <iamsouravdaswade420@gmail.com>
2021-04-17 15:06:56 +0530

    Header

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+  <header>
+    <div class="header">
+        <div class="logo__header">
+          <img src="../assets/slogo.png" alt="header__log"/>
+        </div>
+
+      <div class="navlink__header">
+          <div class="link__container">
+              <a href="#"><div class="nav__link">Premium</div></a>
+              <a href="#"><div class="nav__link">Support</div></a>
+              <a href="#"><div class="nav__link">Download</div></a>|
+          </div>
+
+      </div>
+      <div class="profile__header">
+        <img src="../assets/profile.png" alt="profile__image">
+        <span>Profile</span>
+      </div>
+    </div>
+  </header>
+
+</template>
+
+<script>
+export default {
+  name:'Header'
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
---
+++ src/components/Hi.vue
@@ -0,0 +1,12 @@
+<template lang="html">
+  <h1>lets create spotify clone</h1>
+</template>
+
+<script>
+export default {
+  name:'Hi'
+}
+</script>
+
+<style lang="css" scoped>
+</style>
